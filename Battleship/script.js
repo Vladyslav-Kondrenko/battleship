934d0caaf5286b128bfbefdf02c77d074c4fe595
Vladyslav-Kondrenko <56740626+Vladyslav-Kondrenko@users.noreply.github.com>
2020-03-09 12:57:40 +0200

    Add files via upload
    Автоматическое выставление кораблей, добавлен фон

--- Battleship/script.js
+++ Battleship/script.js
@@ -7,28 +7,88 @@ const again = document.getElementById('again');
 const header = document.querySelector('.header');
 
 const game = {
-  ships: [
-      {
-        location: ['11', '12', '13', '14'],
-        hit: ['', '', '','']
-      },
-      {
-        location: ['36', '37', '38'],
-        hit: ['', '', '']
-      },
-      {
-        location: ['44', '54'],
-        hit: ['', '']
-      },
-      {
-        location: ['87'],
-        hit: ['']
-      },
+  ships: [],
+  shipCount: 0,
+  optionShip: {
+    count: [1,2,3,4],
+    size: [4,3,2,1]
+  },
+  collision: new Set(),
+  generateShip() {
+    for (let i = 0; i < this.optionShip.count.length; i++) {
+      for ( let j = 0; j < this.optionShip.count[i]; j++ ) {
+        const size = this.optionShip.size[i];
+        const ship = this.generateOptionsShip(size);
+        this.ships.push(ship);
+        this.shipCount++;
+
+      }
+    }
+  },
+  generateOptionsShip(shipSize) {
+    const ship = {
+      hit: [],
+      location: [],
+    };
+
+    const directon = Math.random() < 0.5;
+    let x, y;
+
+    if (directon) {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * (10 - shipSize));
+    } else {
+      x = Math.floor(Math.random() * (10 - shipSize));
+      y = Math.floor(Math.random() * 10);
+    }
+
+    for ( let i = 0; i < shipSize; i++){
+      if(directon) {
+        ship.location.push(x + '' + (y + i))
+      } else {
+        ship.location.push((x + i) + '' + y)
+        }
+        ship.hit.push('');
+      }
+
+      if (this.checkCollision(ship.location)) {
+        return this.generateOptionsShip(shipSize);
+      }
+
+      this.addCollision(ship.location);
+      
+
+      return ship;
+    },
+    checkCollision(location) {
+      for (const coord of location) {
+        if (this.collision.has(coord)){
+          return true;
+        }
+      }
+    },
+    addCollision(location) {
+      for(let i = 0; i < location.length; i++) {
+        const startCoordX = location[i][0] - 1;
+        for(let j = startCoordX; j < startCoordX + 3; j++ ) {
+          const startCoordY = location [i][1] - 1
+          for(let z = startCoordY; z < startCoordY + 3; z++){
+            if( j >= 0 && j < 10 && z >= 0 && z < 10){
+              const coord = j + '' + z;
+
+              this.collision.add(coord);
+  
+
+            }
 
 
-    ],
-    shipCount: 4,
-};
+          }
+        }
+      }    
+    }
+  };
+
+
 
 const play = {
     record: localStorage.getItem('seaBattleRecord') || 0,
@@ -93,7 +153,7 @@ const fire = (event) => {
             }
             game.shipCount -= 1;
 
-            if( game.shipCount < 1){
+            if( !game.shipCount ){
               header.textContent = 'Хорошая работа, Капитан';
               header.style.color = 'green';
               
@@ -114,9 +174,14 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
-
+    game.generateShip();
     again.addEventListener('click', () =>{
       location.reload();
+    });
+    record.addEventListener('dblclick', () => {
+      localStorage.clear();
+      play.record = 0;
+      play.render();
     });
 };
 init();
